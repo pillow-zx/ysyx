8261e69148f1a52ad281e7600234911188a8cca9
waysorry <waysorrypub@gmail.com>
2025-07-12 21:39:30 +0800

    重新编写了通用寄存器堆OC

--- npc/vsrc/regfile.v
+++ npc/vsrc/regfile.v
@@ -1,22 +1,24 @@
-module regfile(
-    input wire clk,
-    input wire [4:0] raddr1,
-    output wire [31:0] rdata1,
-    input wire [4:0] raddr2,
-    output wire [31:0] rdata2,
-    input wire we,
-    input wire [4:0] waddr,
-    input wire [31:0] wdata
-);
 
-reg [31:0] rf[31:0];
+module RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
+        input wire clk,
+        input wire [ADDR_WIDTH-1:0] raddr1,
+        output wire [DATA_WIDTH-1:0] rdata1,
+        input wire [ADDR_WIDTH-1:0] raddr2,
+        output wire [DATA_WIDTH-1:0] rdata2,
+        input wire we,
+        input wire [ADDR_WIDTH-1:0] waddr,
+        input wire [DATA_WIDTH-1:0] wdata
+    );
 
-always @(posedge clk) begin
-    if (we)
-        rf[waddr] <= wdata;
-end
+    reg [DATA_WIDTH-1:0] regfile [(1<<ADDR_WIDTH)-1:0];
 
-assign rdata1 = (raddr1 == 5'b0) ? 32'b0 : rf[raddr1];
-assign rdata2 = (raddr2 == 5'b0) ? 32'b0 : rf[raddr2];
+    always @(posedge clk) begin
+        if (we) begin
+            regfile[waddr] <= wdata;
+        end
+    end
+
+    assign rdata1 = (raddr1 == 0) ? 0 : regfile[raddr1];
+    assign rdata2 = (raddr2 == 0) ? 0 : regfile[raddr2];
 
 endmodule
