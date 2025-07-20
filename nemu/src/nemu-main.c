5449a38a34da9c7058e4e5f4869df4a91da22483
waysorry <waysorrypub@gmail.com>
2025-07-20 19:01:15 +0800

    delete some useless codes

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,36 +30,6 @@ int main(int argc, char *argv[]) {
     /* 将客户程序读入到客户计算机中 */
     init_monitor(argc, argv);
 #endif
-    // unsigned wrong = 0;
-    // FILE *fp = fopen("/home/waysorry/ysyx/ysyx-workbench/nemu/tools/gen-expr/build/input", "r");
-    // if (fp == NULL) {
-    //     fprintf(stderr, "Failed to open input file.\n");
-    //     return 1;
-    // }
-    // char *temp = (char *)malloc(1000 * sizeof(char));
-    // while (fgets(temp, 1000, fp) != NULL) {
-    //     char *temps = strtok(temp, " ");
-    //     if (temps == NULL) {
-    //         continue;
-    //     }
-    //     char *exprs = temps + strlen(temps) + 1;
-    //     exprs[strlen(exprs) - 1] = '\0'; // Remove newline character at the end
-    //     word_t result;
-    //     sscanf(temps, "%u", &result);
-    //     bool success = true;
-    //     word_t res = expr(exprs, &success);
-    //     if (!success) {
-    //         printf("Error in expression: %s\n", exprs);
-    //     } else if (res != result) {
-    //         printf("Expression: %s, Expected: %u, Got: %u\n", exprs, result, res);
-    //         wrong++;
-    //     } else {
-    //         printf("Expression: %s, Expected: %u, Got: %u\n", exprs, result, res);
-    //     }
-    // }
-    // printf("Wrong: %u\n", wrong);
-    // fclose(fp);
-    // return 0;
 
     /* Start engine. */
     engine_start();
